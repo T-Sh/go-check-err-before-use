e159434ebe0324ed73821b3c9a598ca5d290caa1
Tatyana <tatyana.shimohina23@gmail.com>
2025-05-24 12:06:02 +0300

    feat: add flag in cmd

--- cmd/go-check-err-before-use/main.go
+++ cmd/go-check-err-before-use/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/T-Sh/go-check-err-before-use/pkg/analyzer"
 	"golang.org/x/tools/go/analysis/singlechecker"
 )
 
 func main() {
-	setting := analyzer.Settings{}
+	var flagDistance = flag.Int("distance", 1, "The acceptable distance between the assignment of an error and its checking")
+	flag.Parse()
+
+	setting := analyzer.Settings{Distance: *flagDistance}
 	singlechecker.Main(analyzer.NewAnalyzer(setting))
 }
